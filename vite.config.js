55c9961ff9dbf3fad15f4790041d2b81406d60ee
MSib <msib.dev@mail.ru>
2025-02-09 19:35:40 +0500

    fix multiple style calls

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,6 @@ export default defineConfig({
     Components({
       resolvers: [PrimeVueResolver()],
     }),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
